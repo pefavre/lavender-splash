4d135b91f02ee816f1a69fa71914f5001ef2f09b
pefavre <pierre.etienne.favre@gmail.com>
2013-03-01 12:17:44 +0100

    Initialized app

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RailsPrelaunchSignup::Application.config.secret_token = '1ba85dd1130cb8eed4b810a598d953d218b51c7a05503474fe24eac65df0f9d9f828a59dcc5adbf1183fba51dcadda34832e04bbd31b44854de0dd3f8cabf45a'
+RailsPrelaunchSignup::Application.config.secret_token = 'aba72ad04cf42b53fa9af57c1bc12f6d9ad1d24c1d14f081741c065f48ae7e6f37cfbf14b4fac83a6d80dfb49102cb6f51859d3561c531e859b9f002ff27a3f'
